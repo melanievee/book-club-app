47ba4ae1ee057bbdead6f82cecb84c6a3a29b7cd
Melanie VanderLugt <melanie@substantial.com>
2018-09-14 10:41:36 -0700

    choose FactoryBot over fixtures, test user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = build(:user)
+  end
+
+  test 'valid factory' do
+    assert @user.valid?
+  end
+
+  test 'email uniqueness' do
+    @user.save
+    duplicate_user = @user.dup
+    assert_not duplicate_user.valid?
+  end
+
+  test 'email presence' do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+  
+  test 'first name presence' do
+    @user.first_name = nil
+    assert_not @user.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'factory_bot_rails'
+require 'faker'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include FactoryBot::Syntax::Methods
 end
