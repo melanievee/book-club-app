5a39a2820d67f8e15c8eef7c2b2f9979b26c2059
Melanie VanderLugt <melanie@substantial.com>
2018-09-17 13:39:05 -0700

    add initial test for api club show route

--- test/controllers/api/clubs_controller_test.rb
+++ test/controllers/api/clubs_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class Api::ClubsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'get single club by id' do
+    club = create(:club)
+    get api_club_url(club)
+
+    response_body = JSON.parse(@response.body)
+
+    assert_response :success
+    assert_equal 'application/json', @response.content_type
+    assert_equal club.name, response_body['name']
+  end
 end
